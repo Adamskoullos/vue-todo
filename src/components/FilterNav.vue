11a6ebcd70c0aab3c671e8662110818b4fc999ea
Adam <adamskoullos@gmail.com>
2021-03-02 16:08:00 +0000

    Home.vue - computed property

---
+++ src/components/FilterNav.vue
@@ -0,0 +1,44 @@
+<template>
+  <nav class="filter-nav">
+      <button @click="updateFilter('all')"
+      :class="{ active: current === 'all' }"
+      >View All</button>
+      <button @click="updateFilter('open')"
+      :class="{ active: current === 'open' }"
+      >Open</button>
+      <button @click="updateFilter('complete')"
+      :class="{ active: current === 'complete' }"
+      >Complete</button>
+  </nav>
+</template>
+
+<script>
+export default {
+    props: ['current'],
+    methods:{
+        updateFilter(by){
+            this.$emit('filter', by)
+        }
+    }
+}
+</script>
+
+<style>
+ .filter-nav button {
+    background: none;
+    border: none;
+    color: #bbb;
+    outline: none;
+    font-size: 12px;
+    text-transform: uppercase;
+    margin-right: 10px;
+    letter-spacing: 1px;
+    font-weight: bold;
+    cursor: pointer;
+  }
+  .filter-nav button.active {
+    color: #555;
+  }
+</style>
+
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="home">
+    <FilterNav @filter="current = $event" :current="current" />
     <div v-if="tasks.length">
-      <div v-for="task in tasks" :key="task.id">
+      <div v-for="task in filteredTasks" :key="task.id">
         <SingleTask :task="task" @delete="removeTask" @complete="completeTask" />
       </div>
     </div>
@@ -10,14 +11,16 @@
 
 <script>
 import SingleTask from '../components/SingleTask.vue'
+import FilterNav from '../components/FilterNav.vue'
 
 export default {
   name: 'Home',
-  components: { SingleTask },
+  components: { SingleTask, FilterNav },
   data(){
     return{
-      tasks: []
-    }
+      tasks: [],
+      current: 'all'
+    };
   },
   mounted(){
     fetch('http://localhost:3000/tasks')
@@ -43,6 +46,18 @@ export default {
       //obj.complete = !obj.complete
       })
     }
+  },
+  computed:{
+    filteredTasks(){
+      if(this.current === 'complete'){
+        return this.tasks.filter(task => task.complete)
+      }
+      if(this.current === 'open'){       
+        return this.tasks.filter(task => !task.complete)
+      }
+      return this.tasks
+    }
   }
 }
+
 </script>
